2ef776689ca6a5af47939974e8a7179d0739e8ab
Christopher Bishop <domingobishop@gmail.com>
2017-11-13 18:51:08 +0000

    header homepage template

--- header.php
+++ header.php
@@ -17,20 +17,33 @@
 
 <body <?php body_class(); ?>>
     <header id="head" class="head">
+        <div class="brand">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <a class="navbar-brand" href="<?php bloginfo('siteurl'); ?>/"
+                           title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>">
+                            <?php bloginfo('name'); ?>
+                        </a>
+                    </div>
+                    <div class="col-md-6 text-right">
+                        <p>
+                            Enquire
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
         <nav class="navbar navbar-default">
-            <div class="container-fluid">
+            <div class="container">
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
                             data-target="#bs-example-navbar-collapse-1"><span class="sr-only">Toggle navigation</span>
                         <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="<?php bloginfo('siteurl'); ?>/"
-                       title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>">
-                        <?php bloginfo('name'); ?>
-                    </a>
                 </div>
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <?php wp_nav_menu(array('menu' => 'primary', 'items_wrap' => '<ul class="nav navbar-nav navbar-right" role="menu">%3$s</ul>', 'container' => false)); ?>
+                    <?php wp_nav_menu(array('menu' => 'primary', 'items_wrap' => '<ul class="nav navbar-nav" role="menu">%3$s</ul>', 'container' => false)); ?>
                 </div>
             </div>
         </nav>
---
+++ page-home.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Template Name: Homepage
+ *
+ */
+get_header(); ?>
+
+    <?php while (have_posts()) : the_post(); ?>
+
+    <main id="main" class="main" role="main">
+        <div class="content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                                <div class="entry-header">
+                                    <h1>
+                                        <?php the_title(); ?>
+                                    </h1>
+                                </div>
+                                <div class="entry-content">
+                                    <?php the_content(); ?>
+                                </div>
+                            </article>
+                        <?php endwhile; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+<?php get_footer(); ?>
